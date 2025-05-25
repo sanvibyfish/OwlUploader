4059326cc8c9f2b6dffb5f82678c8f231107107a
Sanvi <sanvibyfish@gmail.com>
2025-05-25 14:41:16 +0700

    Initial Commit

---
+++ OwlUploader/OwlUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwlUploaderApp.swift
+//  OwlUploader
+//
+//  Created by Sanvi Lu on 2025/5/25.
+//
+
+import SwiftUI
+
+@main
+struct OwlUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
